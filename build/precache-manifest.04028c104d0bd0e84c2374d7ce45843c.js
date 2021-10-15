b2bcda27fd40a8263b0bf9ea43a181d8bb651965
Kartik Pathak <55117106+KP-MIT@users.noreply.github.com>
2021-10-15 16:04:11 +1100

    Deployment commit

---
+++ build/precache-manifest.04028c104d0bd0e84c2374d7ce45843c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a31a4ffa3f84674b74b7bd08ae9e1d9",
+    "url": "/React-Crypto-App.git/index.html"
+  },
+  {
+    "revision": "84a44ab2fc51433fd562",
+    "url": "/React-Crypto-App.git/static/css/2.da8127bb.chunk.css"
+  },
+  {
+    "revision": "93949632893fdefab2f5",
+    "url": "/React-Crypto-App.git/static/css/main.a9b67a56.chunk.css"
+  },
+  {
+    "revision": "84a44ab2fc51433fd562",
+    "url": "/React-Crypto-App.git/static/js/2.09db4edb.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/React-Crypto-App.git/static/js/2.09db4edb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93949632893fdefab2f5",
+    "url": "/React-Crypto-App.git/static/js/main.e002a9a2.chunk.js"
+  },
+  {
+    "revision": "29dfe3980bd599125490",
+    "url": "/React-Crypto-App.git/static/js/runtime-main.1e4f87f9.js"
+  }
+]);
