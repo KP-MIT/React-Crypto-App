747c9410a675498d97c447b8e1f219bdabb2f771
Kartik Pathak <55117106+KP-MIT@users.noreply.github.com>
2021-10-21 16:47:52 +1100

    react-pagination module integrated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
-import {Coin, Pagination, Cryptodetails} from './components';
+import {Coin, Cryptodetails} from './components';  // import Pagination for manually implemented Pagination js file
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Loader from "react-loader-spinner";
 import { Typography } from 'antd';
 import { Route } from 'react-router-dom';
+import ReactPaginate from 'react-paginate'; // using react-pagination package
 
 
 const App = () => {
@@ -26,9 +27,13 @@ const App = () => {
   const [coins, setCoins] = useState([test]);
   const [search, setSearch] = useState('');
   const [loading, setLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
+  //const [searching, setSearching] = useState(false);
+
+  // const [currentPage, setCurrentPage] = useState(1);  // manualPagination
   const [postsPerPage] = useState(10);
-  const [searching, setSearching] = useState(false);
+  const [pageNumber, setPageNumber] = useState(0);
+  const pagesVisited = pageNumber * postsPerPage;
+  
 
   useEffect(() => {
 
@@ -46,26 +51,7 @@ const App = () => {
 
   }, []);
 
-  const handleChange = e => {
-    setSearch(e.target.value);
-    setSearching(true);
-  };
-
-  // Get current coins
-  if (searching) {
-    setCurrentPage(1);
-    setSearching(false);
-  }
-
-  const indexOfLastCoin = currentPage * postsPerPage;
-  const indexOfFirstCoin = indexOfLastCoin - postsPerPage;
-  const currentCoins = coins.filter(coin =>
-    coin.name.toLowerCase().includes(search.toLowerCase())
-  ).slice(indexOfFirstCoin, indexOfLastCoin);
-
-  // Change page
-  const paginate = pageNumber => setCurrentPage(pageNumber)
-  if (loading) {
+  { if (loading) {
     const style = { position: "fixed", top: "50%", left: "50%",
      transform: "translate(-50%, -50%)" };
 
@@ -76,6 +62,47 @@ const App = () => {
     height={"100%"} 
     width={"100%"}  /> 
     );
+  } }
+
+  const handleChange = e => {
+    setSearch(e.target.value);
+    //setSearching(true);
+  };
+
+  // Get current coins with or without manual pagination( uncomment lines )
+  // const indexOfLastCoin = currentPage * postsPerPage;
+  // const indexOfFirstCoin = indexOfLastCoin - postsPerPage;
+  const currentCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase()));
+  // .slice(indexOfFirstCoin, indexOfLastCoin);
+
+  const displayCoins = currentCoins
+  .slice(pagesVisited, pagesVisited + postsPerPage)
+  .map((coin) => {
+    return (
+      <div>
+      <Coin
+        key={coin.id}
+        coinId={coin.id}
+        name={coin.name}
+        price={coin.current_price}
+        symbol={coin.symbol}
+        marketcap={coin.total_volume}
+        volume={coin.market_cap}
+        image={coin.image}
+        priceChange={coin.price_change_percentage_24h}
+        loading={loading}
+      />
+      </div>
+    );
+  });
+
+  // Change page for manual pagination
+  // const paginate = pageNumber => setCurrentPage(pageNumber)
+
+  const pageCount = Math.ceil(currentCoins.length / postsPerPage);
+  const onPageChange = ({selected}) => {
+    setPageNumber(selected);
   }
 
   return (
@@ -95,6 +122,10 @@ const App = () => {
           />
         </form>
       </div>
+      
+      {/* 
+      Uncomment for manual pagination.
+      
       {currentCoins?.map(coin => {
         return (
           <div>
@@ -112,13 +143,34 @@ const App = () => {
           />
           </div>
         );
-      })}
+      })} */}
+
+
+      {displayCoins}
+      <ReactPaginate 
+        previousLabel={"Previous"}
+        nextLabel={"Next"} 
+        pageCount={pageCount}
+        onPageChange={onPageChange}
+        containerClassName={"paginationBttns"}
+        previousLinkClassName={"previousBttns"}
+        nextLinkClassName={"nextBttns"}
+        disabledClassName={"paginationDisabled"}
+        activeClassName={"paginationActive"}
+      />
+
+
+      {/* 
+      manual pagination container
+
       <Pagination
             postsPerPage={postsPerPage}
             totalPosts={coins.length}
             paginate={paginate}
           />
-      
+       */}
+
+
       <div className="footer">
         <Typography.Title level={5}>
               All Rights Reserved @Cryptoscout Inc. 
